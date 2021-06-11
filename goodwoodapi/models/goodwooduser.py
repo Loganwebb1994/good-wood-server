d6620c360d202de6b06b29e43248ff4e257c42c3
Loganwebb1994 <loganwebb1994@gmail.com>
2021-06-11 11:01:08 -0500

    sending user with drop

--- goodwoodapi/models/goodwooduser.py
+++ goodwoodapi/models/goodwooduser.py
@@ -7,12 +7,5 @@ class GoodWoodUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=50)
 
-    @property
-    def first_name(self):
-      return self.user.first_name
 
-    @property
-    def last_name(self):
-      return self.user.last_name
-    
 
