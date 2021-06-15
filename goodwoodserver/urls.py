24a77f2a62c44ab5b437d42770fb93e1b82a6d22
Loganwebb1994 <loganwebb1994@gmail.com>
2021-06-15 14:38:05 -0500

    ?

--- goodwoodserver/urls.py
+++ goodwoodserver/urls.py
@@ -13,6 +13,7 @@ router.register(r'profiles', ProfileView, 'profile')
 
 urlpatterns = [
     path('register', register_user),
+    path('admin/', admin.site.urls),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls))
